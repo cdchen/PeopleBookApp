9da6e89f662282a7a356246e3d812414760d5216
Cd Chen <cdchen@nicestudio.com.tw>
2012-09-06 15:08:23 +0800

    Initial Commit

---
+++ PeopleBookApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PeopleBookApp
+//
+//  Created by Cd Chen on 12/9/6.
+//  Copyright (c) 2012年 Cd Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PeopleBookApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PeopleBookApp
+//
+//  Created by Cd Chen on 12/9/6.
+//  Copyright (c) 2012年 Cd Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
